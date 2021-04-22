96d8915e580368f56913b16f282dd1c236863b53
Lincoln Chow <lincoln995623@gmail.com>
2021-04-22 19:11:07 +0800

    Add a script to calculate the avg. of 100 prediction result

---
+++ cal.py
@@ -0,0 +1,59 @@
+# Import the libraries
+import json
+from matplotlib import pyplot as plt
+import pandas as pd
+
+with open('setting.json') as f:
+    setting = json.load(f)
+
+dataset = pd.read_csv("./dataset/{0}.csv".format(setting["file"]))
+
+arr = []
+
+for i in range(100):
+
+    if setting["file"] != "corn_3m":
+        path = "./result/{0}/{1}-{2}-{3}.csv".format(
+            setting["file"], setting["training_rate"], setting["epochs"], i)
+    else:
+        path = path = "./result/{0}/{1}-{2}/{1}-{2}-{3}.csv".format(
+            setting["file"], setting["training_rate"], setting["epochs"], i)
+    df = pd.read_csv(path, encoding='UTF-8', low_memory=False)
+    arr.append(df)
+
+
+close = arr[0]["Close"]
+lstm = arr[0]["LSTM"]
+rnn = arr[0]["RNN"]
+slr = arr[0]["SLR"]
+for i in range(1, 100):
+    lstm += arr[i]["LSTM"]
+    rnn += arr[i]["RNN"]
+    slr += arr[i]["SLR"]
+
+length = 100
+start = len(dataset)-len(close)
+
+# process the data
+export = pd.DataFrame()
+pd.options.display.float_format = "{:.2f}".format
+export["Date"] = dataset[start:]["Date"].values
+export["Close"] = close
+export["LSTM"] = lstm/length
+export["RNN"] = rnn/length
+export["SLR"] = slr/length
+print(export)
+# plot the figure
+plt.figure(figsize=(16, 8))
+plt.title('result')
+plt.xlabel('Date', fontsize=18)
+plt.ylabel('Close', fontsize=18)
+# plt.plot(train['Close'])
+plt.plot(export['Date'], export[['Close', 'LSTM', 'RNN', 'SLR']])
+plt.legend(['Actually data', 'Predictions(LSTM)',
+            'Predictions(RNN)', 'Predictions(SLR)'], loc='lower right')
+# plt.show()
+
+# save as csv
+log_path = "./result/{0}/avg_predict.csv".format(setting["file"])
+export.to_csv(log_path, float_format='%.2f')
